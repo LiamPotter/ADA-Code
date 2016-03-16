e88e3a03bb33644d72ac4d90accc02c1e065492c
LiamPotter <liamp62@gmail.com>
2016-03-16 19:36:58 +1000

    3 Updates
    Merged updated Database code, implemented switching menus with the
    bumpers and started work on controlling the menu with a controller

---
+++ UI/SelectMenuScripts/EncyclopediaTabController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EncyclopediaTabController : MonoBehaviour {
+
+    public SelectMenuMain selectMM;
+    public DataBaseMain dbMain;
+    public RectTransform currentUIPanel;
+	void Start ()
+    {
+        if (selectMM == null)
+            selectMM = transform.parent.GetComponent<SelectMenuMain>();
+        if (dbMain == null)
+            dbMain = FindObjectOfType<DataBaseMain>();
+	}
+		
+	void Update ()
+    {
+        if (selectMM.currentTab == this.gameObject)
+        {
+            MoveUIPanel(currentUIPanel, Input.GetAxis("RightStickY"), selectMM, dbMain);
+        }
+	}
+    public static void MoveUIPanel(RectTransform uipanel, float direction, SelectMenuMain smm,DataBaseMain dbm)//uipanel is the object that will move, if direction is 0 move up, if 1 move down
+    {
+        
+    }
+}
---
+++ UI/SelectMenuScripts/SelectMenuMain.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectMenuMain : MonoBehaviour {
+
+    public int selectedTab; //the int value that changes on bumper press
+    public GameObject currentTab; //this is changed depending the selectedTab int
+    public GameObject objectivesTab, encyclopediaTab, mapTab; //these are the three actual ui objects in the world space
+    void Start()
+    {
+        //setting the selected object at the start of the scene
+        selectedTab = 0;
+        setCurrentTab();
+    }
+
+    void Update()
+    {
+        //decreasing the selectedTab int by one to cycle left through the menus
+        if (Input.GetButtonDown("LeftBumper"))
+        {
+            if (selectedTab != 0)
+                selectedTab = selectedTab - 1;
+            else selectedTab = 2;
+            setCurrentTab();
+        }
+        //increasing the selectedTab int by one to cycle right through the menus
+        if (Input.GetButtonDown("RightBumper"))
+        {
+            if (selectedTab != 2)
+                selectedTab = selectedTab + 1;
+            else selectedTab = 0;
+            setCurrentTab();
+        }
+    }
+    //this is the static method in charge of setting each objects position in the heirarchy
+    public static void MoveInHeirarchy(int delta, Transform toMove)
+    {
+        int index = toMove.GetSiblingIndex();
+        toMove.SetSiblingIndex(index + delta);
+    }
+    //this is the public method that syncs the selectedTab int variable to the currentTab GameObject variable
+    public void setCurrentTab()
+    {
+        switch (selectedTab)
+        {
+            case 2:
+                currentTab = mapTab;
+                MoveInHeirarchy(3, currentTab.transform);
+                break;
+            case 1:
+                currentTab = encyclopediaTab;
+                MoveInHeirarchy(3, currentTab.transform);
+                break;
+            default:
+                currentTab = objectivesTab;
+                MoveInHeirarchy(3, currentTab.transform);
+                break;                         
+        }
+    }
+}
